c206400d3d2afe96bc980cb7d527aecfca886933
yunshu-66 <3132992431@qq.com>
2026-07-13 14:55:56 +0800

    Add files via upload

---
+++ day3ti.py
@@ -0,0 +1,13 @@
+name='yunshu'
+age=21
+print("我叫",name,",今年",age,"岁")
+n=1
+if n%2==0:
+    print("n是偶数")
+else:
+    print("n是奇数")
+for i in range(1,11):
+    print(i)
+for i in range(11):
+    if i>0:
+        print(i)
