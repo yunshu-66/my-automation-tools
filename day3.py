656a2a0a7c8f948285ca837b9106805ea6362573
yunshu-66 <3132992431@qq.com>
2026-07-14 17:19:21 +0800

    add day3 practice

---
+++ day3.py
@@ -0,0 +1,14 @@
+name='yunshu'
+age='21'
+print("我叫",name,"今年",age,"岁")
+score=50
+if score>=60:
+    print("及格")
+else:
+    print("不及格")
+for i in range(5):
+    print("第",i,"次")
+total=0
+for i in range(1,100):
+    total=total+i
+print("1加到100等于",total)
